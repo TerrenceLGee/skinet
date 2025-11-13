f04cac5421ccf2e0c6f5ae17273ebfeeb13fa008
terrence <mrgee1978@proton.me>
2025-11-12 22:54:29 -0500

    End of section 14.

--- API/Program.cs
+++ API/Program.cs
@@ -53,6 +53,7 @@ var app = builder.Build();
 app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader()
 .AllowAnyMethod()
+.AllowCredentials()
 .WithOrigins("http://localhost:4200","https://localhost:4200"));
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<AppUser>();
