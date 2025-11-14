1a550b5cfc1bbc6bfadcab65a97bd292242aa499
terrence <mrgee1978@proton.me>
2025-11-13 22:50:55 -0500

    Hope to finish section 16 tomorrow

--- Core/Entities/ShoppingCart.cs
+++ Core/Entities/ShoppingCart.cs
@@ -1,9 +1,10 @@
-using System.Reflection;
-
 namespace Core.Entities;
 
 public class ShoppingCart
 {
     public required string Id { get; set; }
     public List<CartItem> Items { get; set; } = [];
+    public int? DeliveryMethodId { get; set; }
+    public string? ClientSecret { get; set; }
+    public string? PaymentIntentId { get; set; }
 }
