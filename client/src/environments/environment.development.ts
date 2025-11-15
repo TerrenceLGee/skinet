b9c4ed9dd7062df0e6dbbbae24bfbac5f2a718a6
terrence <mrgee1978@proton.me>
2025-11-14 22:22:45 -0500

    End of section 16

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51STCmvD8oQ9A8jg7k7lR2w45ZiFNuUCwyAjeA4HfCBZ6x0TbZ2vIYpOR1oPywoSO3gRV0EJlwDpPHKHe9D5kiQxT00Pf0nCI2O'
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51STCmvD8oQ9A8jg7k7lR2w45ZiFNuUCwyAjeA4HfCBZ6x0TbZ2vIYpOR1oPywoSO3gRV0EJlwDpPHKHe9D5kiQxT00Pf0nCI2O'
 };
